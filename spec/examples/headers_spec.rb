9e616241f39d53cf25caf8294f452929aa9826a5
Martin Bilski <martinb@Martin-Bilskis-MacBook-Air.local>
2011-09-05 12:12:15 +0200

    Example spec showing custom request headers & response header matching.

---
+++ spec/examples/headers_spec.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), "../spec_helper")
+require File.join(File.dirname(__FILE__), "sample_actions")
+
+describe CustomHeader, :cramp => true do
+  def app
+    CustomHeader
+  end
+
+  it "should render the value of the custom header" do
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should respond_with :body => /^SAMPLE VALUE$/
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should respond_with :body => "SAMPLE VALUE"
+  end
+  
+  it "should include the custom header in response headers" do
+    # Exact match using string & regex.
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should respond_with :headers => {"Custom-Header" => "SAMPLE VALUE"}
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should respond_with :headers => {"Custom-Header" => /^SAMPLE VALUE$/}
+
+    # Header field names are case insensitive - use regex match:
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should respond_with :headers => {/Custom\-Header/i => "SAMPLE VALUE"}
+
+    # Negative match.
+    get("/", :headers => {"Custom-Header" => "SAMPLE VALUE"}).should_not respond_with :headers => {"Custom-Header" => "ANOTHER VALUE"}
+  end
+end
